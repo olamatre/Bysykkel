43d9beef71568e389b985f3aed0e2e2ca99fd1e8
omatrebym <ola.matre@bym.oslo.kommune.no>
2026-06-22 22:37:52 +0200

    Flett status inn i stativ

--- backend/Api/Concepts/Stativ/Queries/HentAlleStativerQuery.cs
+++ backend/Api/Concepts/Stativ/Queries/HentAlleStativerQuery.cs
@@ -11,7 +11,16 @@ public record StativViewModel(
     string Adresse,
     double Latitude,
     double Longitude,
-    int Kapasitet
+    int Kapasitet,
+    StativStatusViewModel Status
+);
+
+public record StativStatusViewModel(
+    int AntallLedigePlasser,
+    int AntallSykler,
+    bool IsRenting,
+    bool IsReturning,
+    DateTimeOffset SistOppdatert
 );
 
 public sealed class HentAlleStativerQueryHandler(IBikeshareClient bikeshareClient)
@@ -21,6 +30,7 @@ public sealed class HentAlleStativerQueryHandler(IBikeshareClient bikeshareClien
     public async Task<IReadOnlyList<StativViewModel>> Handle(GetStativQuery query, CancellationToken cancellationToken = default)
     {
         var stationInformation = await bikeshareClient.GetStationInformationAsync(cancellationToken);
+        var stationStatusInformation = await bikeshareClient.GetStationStatusAsync(cancellationToken);
         return stationInformation.Data.Stations
             .Select(station => new StativViewModel(
                 station.StationId,
@@ -28,7 +38,15 @@ public sealed class HentAlleStativerQueryHandler(IBikeshareClient bikeshareClien
                 station.Address,
                 station.Lat,
                 station.Lon,
-                station.Capacity))
+                station.Capacity,
+                new StativStatusViewModel(
+                    stationStatusInformation.Data.Stations.FirstOrDefault(s => s.StationId == station.StationId)?.NumBikesAvailable ?? 0,
+                    stationStatusInformation.Data.Stations.FirstOrDefault(s => s.StationId == station.StationId)?.NumDocksAvailable ?? 0,
+                    stationStatusInformation.Data.Stations.FirstOrDefault(s => s.StationId == station.StationId)?.IsRenting ?? false,
+                    stationStatusInformation.Data.Stations.FirstOrDefault(s => s.StationId == station.StationId)?.IsReturning ?? false,
+                    DateTimeOffset.FromUnixTimeSeconds(stationStatusInformation.Data.Stations.FirstOrDefault(s => s.StationId == station.StationId)?.LastReported ?? 0)
+                )
+            ))
             .ToArray();
     }
 }
